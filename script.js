5b76a0d2c8cd5a64d9f5a9b4c7f0901cdcd1e071
Gustav Burchardt <gustavburchardt@gmail.com>
2018-11-16 12:35:31 +0100

    Add option to include source

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var fileWidth = 1
 var fileHeight = 1
 var source
 var img
+var includeSource = true
 
 function changeFile(files) {
   var reader = new FileReader()
@@ -31,34 +32,40 @@ function applyImage(source, callback) {
 }
 
 function applyOptions(options) {
-  if (options.width) {
+  if (options.width !== undefined) {
     console.log('Applying width ' + options.width)
 
     spriteWidth = parseInt(options.width, 10)
   }
 
-  if (options.height) {
+  if (options.height !== undefined) {
     console.log('Applying height ' + options.height)
 
     spriteHeight = parseInt(options.height, 10)
   }
 
-  if (options.fileWidth) {
+  if (options.fileWidth !== undefined) {
     console.log('Applying fileWidth ' + options.fileWidth)
 
     fileWidth = parseInt(options.fileWidth, 10)
   }
 
-  if (options.fileHeight) {
+  if (options.fileHeight !== undefined) {
     console.log('Applying fileHeight ' + options.fileHeight)
 
     fileHeight = parseInt(options.fileHeight, 10)
   }
 
-  if (options.source) {
+  if (options.source !== undefined) {
     console.log('Applying source ' + options.source)
 
     source = options.source
+  }
+
+  if (options.includeSource !== undefined) {
+    console.log('Applying source ' + options.includeSource)
+
+    includeSource = options.includeSource
   }
 }
 
@@ -82,8 +89,19 @@ function renderFlipped () {
 
   var canvas = document.createElement('canvas')
   canvas.width = img.width
-  canvas.height = img.height
+  if (includeSource) {
+    canvas.height = img.height * 2
+  } else {
+    canvas.height = img.height
+  }
   var ctx = canvas.getContext('2d')
+
+  var offsetY = 0
+
+  if (includeSource) {
+    ctx.drawImage(img, 0, 0)
+    offsetY = img.height
+  }
 
   for (var x = 0; x < img.width; x += spriteWidth) {
     ctx.save()
@@ -96,7 +114,7 @@ function renderFlipped () {
       spriteWidth,
       spriteHeight,
       0,
-      0,
+      offsetY,
       spriteWidth,
       spriteHeight
     )
